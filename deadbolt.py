b547d68acc3de55cd3d93a64e0fed3eb4b634a8d
Rocco Febbo <febbo87@gmail.com.com>
2024-10-21 13:11:59 -0400

    update

--- deadbolt.py
+++ deadbolt.py
@@ -4,13 +4,13 @@ import requests
 import regex as re
 import json
 
-from dli_heros import load_local_heros
+from dli_heros import load_local_heros, download_hero_info
 from dli_items import download_item_info
 from hero import hero
 
 
 heros = load_local_heros()
-
+# download_hero_info()
 download_item_info()
 
 for h in heros:
